aee946e0d221f18b3c6227c34d8fbe0060f4394b
brant9527 <398707650@qq.com>
2018-08-21 14:56:37 +0800

    添加0000

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ let app = require('./router/index')
 require('./mongodb/index')
 
 
-var server = app.listen(8080, function () {
+var server = app.listen(8080,'0.0.0.0', function () {
 
     var host = server.address().address
+    console.log(JSON.stringify(server.address()))
     var port = server.address().port
 
-    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+    console.log(`应用实例，访问地址为 http://${host}:${port}`, host, port)
 
 })
